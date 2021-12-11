01f7398aea149490253c50efb10dffb7fede37a8
Dan Grover <dogrover@gmail.com>
2021-12-11 12:32:10 -0800

    Add single filename from CLI

--- counter.go
+++ counter.go
@@ -7,7 +7,23 @@ import (
 )
 
 func main() {
-	file, err := os.Open("test_data.txt")
+	// Exit halts execution before deferred functions, log flushes, and other
+	// cleanup. So keep those in a separate function.
+	os.Exit(run())
+}
+
+func run() int {
+	// CHeck that we've been given at least one file to process
+	numArgs := len(os.Args[1:])
+	if numArgs < 1 {
+		fmt.Fprintln(os.Stderr, "No filenames given")
+		return 1
+	} else if numArgs > 1 {
+		fmt.Fprintln(os.Stderr, "Multiple filenames given")
+		return 1
+	}
+
+	file, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -21,4 +37,6 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
+
+	return 0
 }
